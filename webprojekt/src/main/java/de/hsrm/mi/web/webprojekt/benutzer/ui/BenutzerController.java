bcecc31131b0ece556b9c5326319efdb770773e2
Meron Tewolde <meron.tewolde@student.hs-rm.de>
2026-05-04 22:52:42 +0200

    Aufgabe 3

--- webprojekt/src/main/java/de/hsrm/mi/web/webprojekt/benutzer/ui/BenutzerController.java
+++ webprojekt/src/main/java/de/hsrm/mi/web/webprojekt/benutzer/ui/BenutzerController.java
@@ -1,21 +1,27 @@
-import org.junit.platform.commons.logging.LoggerFactory;
+package de.hsrm.mi.web.webprojekt.benutzer.ui;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class BenutzerController {
 
-    @GetMapping("/admin/benutzer/{loginname}")
+    
 
     Logger logger = LoggerFactory.getLogger(BenutzerController.class);
 
-    public String getMethodName(@PathVariable String loginname, Model model) {
-        logger.info(GET/admin/benutzer/{}, loginname);
+    @GetMapping("/admin/benutzer/{loginname}")
+    public String benutzerBearbeiten(@PathVariable String loginname, Model model) {
+        logger.info("GET /admin/benutzer/{}", loginname);
 
         model.getAttribute(loginname);
+
+        return "benutzer/bearbeiten";
 
     }
     
